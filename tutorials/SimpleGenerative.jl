9189891f3f322657a6cadbb32a40366163d14fb6
slwu89 <10673535+slwu89@users.noreply.github.com>
2023-11-29 17:24:18 -0800

    text for synthetic example on continuous y

--- tutorials/SimpleGenerative.jl
+++ tutorials/SimpleGenerative.jl
@@ -130,44 +130,25 @@
 
 # ## Synthetic Data Example with Continuous $y$
 
+# We now present an example of finding cost-efficient generative designs using synthetic data using the CEEDesigns.jl package.
+# 
+# First we load necessary packages.
+
 using CEEDesigns, CEEDesigns.GenerativeDesigns
 using DataFrames
 using ScientificTypes
 import Distributions
+using Copulas
 using POMDPs, POMDPTools, MCTS
 using Plots, StatsPlots, StatsBase
 
-# --------------------------------------------------------------------------------
-# In this file we want to develop 2 simple examples of using the generative designs
-# solver. 
-#   * One for continuous outcomes using the Variance based `uncertainty` function.
-#   * One for discrete outcomes using the Entropy based `uncertainty` function. 
+# ### Synthetic Data
 
-# --------------------------------------------------------------------------------
-# 
-# First example is for the continuous outcome.
-# 
-# --------------------------------------------------------------------------------
-
-# --------------------------------------------------------------------------------
-# We want to generate synthetic data (see https://scikit-learn.org/stable/datasets/sample_generators.html#generators-for-regression)
-# We'll sample the predictors from a 4D Gaussian copula, so that there is specified covariance
-# between the predictors. Otherwise the similarity metric will not be very meaningful, because if all
-# predictors are independent similarity will essentially be uniformly distributed.
-
-# these are the 3 sample functions for synthetic regression data from
-# https://scikit-learn.org/stable/datasets/sample_generators.html#generators-for-regression
-
-make_friedman1 = function (n, noise = 0)
-    X = DataFrame(rand(Float64, (n, 5)), :auto)
-    ϵ = noise > 0 ? rand(Distributions.Normal(0, noise), size(X, 1)) : 0
-    X.y = @. 10 * sin(π * X[:, 1] * X[:, 2]) +
-       20 * (X[:, 3] - 0.5)^2 +
-       10 * X[:, 4] +
-       5 * X[:, 5] +
-       ϵ
-    return X
-end
+# We use the "Friedman #2" method to make synthetic data for a regression problem from [scikit-learn](https://scikit-learn.org/stable/datasets/sample_generators.html#generators-for-regression).
+# It considers $m=4$ features which predict a continuous $y$ via some nonlinear transformations. The marginal
+# distributions of each feature are given by the scikit-learn documentation. We additionally use a Gaussian copula
+# to induce a specified correlation structure on the features to illustrate the difference between Euclidean and Mahalanobis
+# distance metrics.
 
 make_friedman2 = function (U, noise = 0)
     size(U, 2) == 4 || error("input U must have 4 columns, has $(size(U,2))")
@@ -193,10 +174,6 @@ make_friedman3 = function (U, noise = 0)
     return X
 end
 
-using Copulas
-
-# sample the "predictors" from a Gaussian copula with specified covariance matrix
-# the strange marginals are from https://scikit-learn.org/stable/datasets/sample_generators.html#generators-for-regression
 ρ12, ρ13, ρ14, ρ23, ρ24, ρ34 = 0.8, 0.5, 0.3, 0.5, 0.25, 0.4
 Σ = [
     1 ρ12 ρ13 ρ14
@@ -210,7 +187,6 @@ X2 = Distributions.Uniform(40 * π, 560 * π)
 X3 = Distributions.Uniform(0, 1)
 X4 = Distributions.Uniform(1, 11)
 
-# generate the copula distribution
 C = GaussianCopula(Σ)
 D = SklarDist(C, (X1, X2, X3, X4))
 
@@ -218,11 +194,11 @@ X = rand(D, 1000)
 
 data = make_friedman3(transpose(X), 0.01)
 
-# We can check that the features in the data have roughly given correlation structure:
+# We can check that the empirical correlation is roughly the same as the specified theoretical values: 
+
 cor(Matrix(data[:, Not(:y)]))
 
-# --------------------------------------------------------------------------------
-# Generate synthetic "historical data"
+# We ensure that our algorithms know that we have provided data of specified types. 
 
 types = Dict(
     :x1 => ScientificTypes.Continuous,
@@ -233,20 +209,25 @@ types = Dict(
 )
 data = coerce(data, types);
 
-# we can see what the outcome looks like against each predictor
+# We may plot each feature $x_{i} \in X = {x_{1},x_{2},x_{3},x_{4}}$ against $y$.
+
 p1 = scatter(data.x1, data.y; legend = false, xlab = "x1")
 p2 = scatter(data.x2, data.y; legend = false, xlab = "x2")
 p3 = scatter(data.x3, data.y; legend = false, xlab = "x3")
 p4 = scatter(data.x4, data.y; legend = false, xlab = "x4")
 plot(p1, p2, p3, p4; layout = (2, 2), legend = false)
 
-# let's see histogram of the outcome
+# And we may also look at a histogram of $y$.
+
 histogram(data.y; legend = false, ylab = "Count", xlab = "y")
 
-# --------------------------------------------------------------------------------
-# set up the distance based method for weights and uncertainty evaluation
+# Next we set up the functions for computing distance, similarity, and uncertainty.
+# In both cases we use the variance as our uncertinaty metric. 
+# By default, Euclidean distance is used as the distance metric. In the second
+# call to `DistanceBased`, we use explicitly use the Mahalanobis distance.
+# In both cases, the squared exponential function is used to convert distances
+# to weights.
 
-# standard quadratic (Euclidean) distance
 (; sampler, uncertainty, weights) = DistanceBased(
     data;
     target = "y",
@@ -254,8 +235,6 @@ histogram(data.y; legend = false, ylab = "Count", xlab = "y")
     similarity = GenerativeDesigns.Exponential(; λ = 5),
 );
 
-# we may also use Mahalanobis distance which takes into account covaraince
-# (; sampler_mahalanobis, uncertainty_mahalanobis, weights_mahalanobis) = DistanceBased(
 (sampler_mh, uncertainty_mh, weights_mh) = DistanceBased(
     data;
     target = "y",
@@ -264,10 +243,18 @@ histogram(data.y; legend = false, ylab = "Count", xlab = "y")
     distance = MahalanobisDistance(; diagonal = 1),
 );
 
-# we may also look at the uncertainty from each marginal distribution
+# We can look at the uncertainty in $y$ for a state where a single
+# feature is "observed" at its mean value. Note that uncertainty is generally higher
+# for the Mahalanobis distance, which makes sense as it takes into account the 
+# non-independence of the features.
+
 data_uncertainties =
     [i => uncertainty(Evidence(i => mean(data[:, i]))) for i in names(data)[1:4]]
 sort!(data_uncertainties; by = x -> x[2], rev = true)
+
+data_uncertainties_mh =
+    [i => uncertainty_mh(Evidence(i => mean(data[:, i]))) for i in names(data)[1:4]]
+sort!(data_uncertainties_mh; by = x -> x[2], rev = true)
 
 p1 = sticks(
     eachindex(data_uncertainties),
@@ -276,11 +263,6 @@ p1 = sticks(
     label = false,
     title = "Uncertainty\n(Euclidean distance)",
 )
-
-data_uncertainties_mh =
-    [i => uncertainty_mh(Evidence(i => mean(data[:, i]))) for i in names(data)[1:4]]
-sort!(data_uncertainties_mh; by = x -> x[2], rev = true)
-
 p2 = sticks(
     eachindex(data_uncertainties_mh),
     [i[2] for i in data_uncertainties_mh];
@@ -291,29 +273,11 @@ p2 = sticks(
 
 plot(p1, p2; layout = (1, 2), legend = false)
 
-# we can view the posterior distribution over the outcome conditioned on the current state
+# We can view the posterior distribution over $y$ conditioned on a state (here arbitrarily set to $x_{3}$).
+
 evidence = Evidence("x3" => mean(data.x3))
 plot_weights = StatsBase.weights(weights(evidence))
 plot_weights_mh = StatsBase.weights(weights_mh(evidence))
-
-# p1 = StatsPlots.density(
-#     data.y, weights=plot_weights, 
-#     legend=false, ylabel="Density", title="q(y|eₛ), uncertainty $(round(uncertainty(evidence), digits=1))\n(Euclidean distance)"
-# )
-# p2 = Plots.histogram(
-#     data.y, weights=plot_weights, 
-#     legend=false, ylabel="Density", title="q(y|eₛ)\n(Euclidean distance)"
-# )
-
-# p3 = StatsPlots.density(
-#     data.y, weights=plot_weights_mh, 
-#     legend=false, ylabel="Density", title="q(y|eₛ), uncertainty $(round(uncertainty(evidence), digits=1))\n(Mahalanobis distance)"
-# )
-# p4 = Plots.histogram(
-#     data.y, weights=plot_weights_mh, 
-#     legend=false, ylabel="Density", title="q(y|eₛ)\n(Mahalanobis distance)"
-# )
-# plot(p1,p2,p3,p4, layout=(2,2), legend=false)
 
 p1 = Plots.histogram(
     data.y;
@@ -332,28 +296,17 @@ p2 = Plots.histogram(
 
 plot(p1, p2; layout = (1, 2), legend = false)
 
-# # we'll set up the costs of each experiment
-# experiments_costs = Dict(
-#     "e1" => (1,1) => ["x1"],
-#     "e2" => (1,1) => ["x2"],
-#     "e3" => (1,1) => ["x3"],
-#     "e4" => (1,1) => ["x4"]
-# )
+# Like static designs, generative designs need to be provided a `DataFrame` assigning to each experiment
+# a cost and time $(m_{e},t_{e})$, and what features each experiment provides observations of.
+# We'll set up the experimental costs such that experiments which have less marginal uncertinaty are more costly
+# We finally add a very expensive "final" experiment which can directly observe the target variable.
 
-# # make a nice dataframe display of the exp costs and what they measure
-# experiments_costs_df = DataFrame(experiment=String[], time=Int[], cost=Int[], measurement=String[]);
-# push!(experiments_costs_df,[[e, experiments_costs[e][1][1], experiments_costs[e][1][2], only(experiments_costs[e][2])] for e in keys(experiments_costs)]...);
-# experiments_costs_df
-
-# we'll set up the experimental costs such that experiments which have less marginal uncertinaty
-# are more costly
 observables_experiments = Dict(["x$i" => "e$i" for i = 1:4])
 experiments_costs = Dict([
     observables_experiments[e[1]] => (i, i) => [e[1]] for
     (i, e) in enumerate(data_uncertainties_mh)
 ])
 
-# add a final very expensive experiment directly on the target variable
 experiments_costs["ey"] = (100, 100) => ["y"]
 
 experiments_costs_df =
@@ -371,10 +324,12 @@ push!(
 );
 experiments_costs_df
 
-# --------------------------------------------------------------------------------
-# find efficient actions
+# We can now find cost-efficient experimental designs for a new entity that has no measurements (`Evidence()`),
+# for 7 different threshold levels of uncertainty, evenly spaced between 0 and 1, inclusive. Our objevtive is to minimize the
+# expected experimental cost for meeting each target threshold.
+# 
+# Internally, for each choice of the uncertainty threshold, an instance of a Markov decision problem in [POMDPs.jl](https://github.com/JuliaPOMDP/POMDPs.jl) is created, and the `POMDPs.solve` is called on the problem. Afterwards, a number of simulations of the decision-making problem is run, all starting with the experimental `state`.
 
-# options
 n_thresholds = 7
 evidence = Evidence()
 solver = DPWSolver(; n_iterations = 500, tree_in_info = true)
@@ -395,6 +350,8 @@ designs = efficient_designs(
     repetitions = repetitions,
     mdp_options = mdp_options,
 );
+
+# We plot the Pareto-efficient actions.
 
 plot_front(designs; labels = make_labels(designs), ylabel = "% uncertainty")
 
