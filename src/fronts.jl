6bbb021a362a798f7097ff7e32f2052187f37a5d
Bíma, Jan <jan.bima@merck.com>
2023-12-01 20:50:24 +0100

    Fixed signature

--- src/fronts.jl
+++ src/fronts.jl
@@ -130,10 +130,11 @@ function make_labels(designs)
 end
 
 """
-    plot_evals(eval, f; ylabel="information measure")
+    plot_evals(evals; f, ylabel="information measure")
 
 Create a stick plot that visualizes the performance measures evaluated for subsets of experiments.
-Argument `evals` should be the output of `evaluate_experiments` and the kwarg `f` (if provided) is a function that
+
+Argument `evals` should be the output of [`evaluate_experiments`](@ref) and the kwarg `f` (if provided) is a function that
 should take as input `evals` and return a list of its keys in the order to be plotted on the x-axis.
 By default they are sorted by length.
 """
